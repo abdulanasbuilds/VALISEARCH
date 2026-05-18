c043e5f898e0d0af126ff7cf78f3b0d7921399ca
Abdul Anas <abdulanasbuilds@gmail.com>
2026-05-18 07:55:43 +0000

    Configure defaultTheme to light for pure premium light-mode layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,8 +46,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
-          enableSystem={true}
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           {children}
